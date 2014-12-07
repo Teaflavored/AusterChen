1dc63903dea5bfa13602b941a591de94fca8c8c7
Auster Chen <Auster.s.chen@gmail.com>
2014-12-07 14:45:01 -0800

    added all css/js files necessary/linked up fonts

--- vendor/assets/javascripts/init.js
+++ vendor/assets/javascripts/init.js
@@ -9,12 +9,12 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			'global':	{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 40 }, viewport: { scalable: false } },
-			'wide':		{ range: '961-1880', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-			'normal':	{ range: '961-1620', href: 'css/style-normal.css', containers: 960, grid: { gutters: 40 } },
-			'narrow':	{ range: '961-1320', href: 'css/style-narrow.css', containers: '100%', grid: { gutters: 20 } },
-			'narrower':	{ range: '-960', href: 'css/style-narrower.css', containers: '100%', grid: { gutters: 20 } },
-			'mobile':	{ range: '-736', href: 'css/style-mobile.css', containers: '100%!', grid: { collapse: true } }
+			'global':	{ range: '*', href: 'assets/style.css', containers: 1400, grid: { gutters: 40 }, viewport: { scalable: false } },
+			'wide':		{ range: '961-1880', href: 'assets/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+			'normal':	{ range: '961-1620', href: 'assets/style-normal.css', containers: 960, grid: { gutters: 40 } },
+			'narrow':	{ range: '961-1320', href: 'assets/style-narrow.css', containers: '100%', grid: { gutters: 20 } },
+			'narrower':	{ range: '-960', href: 'assets/style-narrower.css', containers: '100%', grid: { gutters: 20 } },
+			'mobile':	{ range: '-736', href: 'assets/style-mobile.css', containers: '100%!', grid: { collapse: true } }
 		},
 		plugins: {
 			layers: {
@@ -46,14 +46,14 @@
 
 		var	$window = $(window),
 			$body = $('body');
-			
+
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
-			
+
 			$window.on('load', function() {
 				$body.removeClass('is-loading');
 			});
-			
+
 		// CSS polyfills (IE<9).
 			if (skel.vars.IEVersion < 9)
 				$(':last-child').addClass('last-child');
@@ -80,7 +80,7 @@
 
 		// Nav.
 			var $nav_a = $('#nav a');
-			
+
 			// Scrolly-fy links.
 				$nav_a
 					.scrolly()
@@ -88,36 +88,36 @@
 
 						var t = $(this),
 							href = t.attr('href');
-						
+
 						if (href[0] != '#')
 							return;
-						
+
 						e.preventDefault();
-						
+
 						// Clear active and lock scrollzer until scrolling has stopped
 							$nav_a
 								.removeClass('active')
 								.addClass('scrollzer-locked');
-					
+
 						// Set this link to active
 							t.addClass('active');
-					
+
 					});
 
 			// Initialize scrollzer.
 				var ids = [];
-				
+
 				$nav_a.each(function() {
-					
+
 					var href = $(this).attr('href');
-					
+
 					if (href[0] != '#')
 						return;
-				
+
 					ids.push(href.substring(1));
-				
+
 				});
-				
+
 				$.scrollzer(ids, { pad: 200, lastHack: true });
 
 	});
